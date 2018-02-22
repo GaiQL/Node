cd8157441a2ac5487a8b8cfe6b999f415602f64a
GaiQL <609100252@qq.com>
2018-02-22 22:03:50 +0800

    2018.2.22

--- Express+MongoDb 商品管理系统/app.js
+++ Express+MongoDb 商品管理系统/app.js
@@ -1,4 +1,7 @@
 var express = require('express');
+var MongoClient = require('mongodb').MongoClient;
+var bodyParse = require('body-parser');
+var DBurl = 'mongodb://127.0.0.1:27017/merManage';
 
 var app = new express;
 
@@ -7,15 +10,62 @@ app.listen(8000);
 app.set('view engine','ejs');
 
 app.use(express.static('public'));
+app.use(bodyParse.urlencoded({extended:false}))
+app.use(bodyParse.json())
+
 
 app.set('view degnic','ejs');
 
-app.get('/',(req,res)=>{
+app.get('/login',(req,res)=>{
   res.render('login');
+  //获取前端发来的用户名密码，查看数据库中是否有匹配
+  //匹配后在数据库中存储session
+  //
 })
 
-app.get('/goods',(req,res)=>{
-  res.render('index');
+app.get('/',(req,res)=>{
+  res.send('hello world!!!!!');
+})
+
+app.post('/goods',(req,res)=>{
+
+  var data = req.body;
+  console.log(data);
+  MongoClient.connect(DBurl,(err,db)=>{
+    if(err){
+      console.log('数据库连接失败');
+    }else{
+      var list = [];
+      var curosr = db.collection('user').find(data);
+
+      //另一种遍历数据的方法
+      curosr.toArray((err,data)=>{
+        if( data.length ){
+          res.render('index');
+        }else{
+          res.send('<script>alert("您输入的账号密码不存在哦...");location.href="/login"</script>')
+        }
+      })
+
+      // curosr.each((err,doc)=>{
+      //   if( err ){
+      //     console.log(err);
+      //   }else{
+      //     if( doc != null ){
+      //       list.push(doc);
+      //     }else{
+      //       if( list.length ){
+      //         res.render('index');
+      //       }else{
+      //         res.send('<script>alert("您输入的账号密码不存在哦...")</script>')
+      //       }
+      //     }
+      //   }
+      // })
+
+    }
+  })
+  console.log(req.body)
 })
 
 app.get('/goodsAdd',(req,res)=>{
