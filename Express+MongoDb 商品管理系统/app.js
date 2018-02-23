8930b4c708de4d5ad5928f8cbe033824ed9abd2c
GaiQL <609100252@qq.com>
2018-02-23 21:20:33 +0800

    2018.2.23

--- Express+MongoDb 商品管理系统/app.js
+++ Express+MongoDb 商品管理系统/app.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var MongoClient = require('mongodb').MongoClient;
 var bodyParse = require('body-parser');
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
+
 var DBurl = 'mongodb://127.0.0.1:27017/merManage';
 
 var app = new express;
@@ -14,7 +17,36 @@ app.use(bodyParse.urlencoded({extended:false}))
 app.use(bodyParse.json())
 
 
-app.set('view degnic','ejs');
+app.use(session({
+  secret: 'Random',          //secret  一个string类型的字符串，作为服务器端生成session的签名。
+  name:'heihei',             //name    返回客户端key的名称，默认为connect.sid,也可以自己设置。
+  resave: false,             //resave  强制保存session,及时session没有变化。  false最好
+  saveUninitialized: true,   //saveUninitialized   强制将未初始化的session值存储。
+  cookie: { secure: false,maxAge:5000000 },   //cookie   设置返回到前端Key的属性，默认值为{paht:'/',httpOnly:true,secure:false,maxAge:null};
+  rolling:true,               //默认false，在每次请求时强行设置cookie，这将重置cookie过期时间。
+  store:new MongoStore({
+      url: 'mongodb://127.0.0.1:27017/sad',
+      touchAfter: 24 * 3600
+  })
+}))
+
+app.use((req,res,next)=>{
+
+  if( req.url == '/login' || req.url == '/dologin' ){
+    next();
+  }else{
+    if( req.session.userInfo ){
+
+      //ejs模板配置全局变量
+      app.locals['name'] = req.session.userInfo.userName
+      next();
+    }else{
+      // res.send('<script>alert("请您先登录");location.href="/login"</script>')
+      res.redirect('/login');
+    }
+  }
+})
+
 
 app.get('/login',(req,res)=>{
   res.render('login');
@@ -23,12 +55,8 @@ app.get('/login',(req,res)=>{
   //
 })
 
-app.get('/',(req,res)=>{
-  res.send('hello world!!!!!');
-})
-
-app.post('/goods',(req,res)=>{
-
+app.set('view degnic','ejs');
+app.post('/dologin',(req,res)=>{
   var data = req.body;
   console.log(data);
   MongoClient.connect(DBurl,(err,db)=>{
@@ -37,41 +65,50 @@ app.post('/goods',(req,res)=>{
     }else{
       var list = [];
       var curosr = db.collection('user').find(data);
+      //去数据库寻找数据的时候，要记得去遍历拿数据。
 
       //另一种遍历数据的方法
       curosr.toArray((err,data)=>{
         if( data.length ){
-          res.render('index');
+          req.session.userInfo = data[0];
+          res.redirect('/goods');   //跳转
+          //session 保存用户信息;
         }else{
           res.send('<script>alert("您输入的账号密码不存在哦...");location.href="/login"</script>')
         }
       })
 
-      // curosr.each((err,doc)=>{
-      //   if( err ){
-      //     console.log(err);
-      //   }else{
-      //     if( doc != null ){
-      //       list.push(doc);
-      //     }else{
-      //       if( list.length ){
-      //         res.render('index');
-      //       }else{
-      //         res.send('<script>alert("您输入的账号密码不存在哦...")</script>')
-      //       }
-      //     }
-      //   }
-      // })
-
     }
   })
-  console.log(req.body)
 })
 
-app.get('/goodsAdd',(req,res)=>{
+app.get('/',(req,res)=>{
+  res.send('hello world!!!!!');
+})
+
+
+app.get('/goods',(req,res)=>{
+
+  res.render('index',{
+    name:req.session.userInfo.userName
+  });
+
+})
+
+app.get('/add',(req,res)=>{
   res.render('add');
 })
 
-app.get('/goodsEdit',(req,res)=>{
+app.get('/edit',(req,res)=>{
   res.render('edit');
+})
+
+
+app.get('/quit',(req,res)=>{
+  req.session.destroy((err)=>{
+    if(!err){
+      console.log('销毁成功');
+      res.redirect('login');
+    }
+  })
 })
